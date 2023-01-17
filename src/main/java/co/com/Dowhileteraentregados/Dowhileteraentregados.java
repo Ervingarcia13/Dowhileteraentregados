e46afec888e89808afeac5f7065053b7fe6d1e59
Ervingarcia13 <ervingarcia13@hotmail.com>
2023-01-17 05:51:57 -0500

    Entrega de Do While

---
+++ src/main/java/co/com/Dowhileteraentregados/Dowhileteraentregados.java
@@ -0,0 +1,4 @@
+package co.com.Dowhileteraentregados;
+
+public class Dowhileteraentregados {
+}
---
+++ src/main/java/co/com/Dowhileteraentregados/Main.java
@@ -0,0 +1,15 @@
+package co.com.Dowhileteraentregados;
+
+public class Main {
+    public static void main(String[]args){
+
+
+        int Numerowhile =2;
+
+        do {
+            System.out.println("El Numero es Menor   " +Numerowhile);
+            Numerowhile++;
+        }while(Numerowhile<3);
+    }
+
+}
